ecb9d116f61607e8cd2d1837fe5ee8e3391792e7
Leon Zawodowiec <leonzawodowiec1532@yahoo.com>
2013-05-31 05:24:06 +0200

    Added config.lua file which not include real-tibia settings

---
+++ config.lua
@@ -0,0 +1,295 @@
+	fragTime = 24
+	sendAfk = true
+	rookLevelTo = 5
+	rookTownId = 1
+	rookSystem = false
+	noDropBless = false
+	accountManager = false
+	namelockManager = true
+	newPlayerChooseVoc = false
+	newPlayerSpawnPosX = 95
+	newPlayerSpawnPosY = 117
+	newPlayerSpawnPosZ = 7
+	newPlayerTownId = 1
+	newPlayerLevel = 1
+	newPlayerMagicLevel = 0
+	generateAccountNumber = false
+	generateAccountSalt = true
+
+	useFragHandler = true
+	redSkullLength = 2 * 24 * 60 * 60
+	blackSkullLength = 4 * 24 * 60 * 60
+	
+	dailyFragsToRedSkull = 7 
+    weeklyFragsToRedSkull = 30 
+    monthlyFragsToRedSkull = 60 
+    
+    dailyFragsToBlackSkull = 15 
+    weeklyFragsToBlackSkull = 50 
+    monthlyFragsToBlackSkull = 80 
+    
+    dailyFragsToBanishment = 40 
+    weeklyFragsToBanishment = 400 
+    monthlyFragsToBanishment = 4000
+	
+	dailyFragsToBanishment = 14
+	weeklyFragsToBanishment = 28
+	monthlyFragsToBanishment = 47
+	blackSkulledDeathHealth = 40
+	blackSkulledDeathMana = 0
+	useBlackSkull = true
+	advancedFragList = true
+
+	notationsToBan = 3
+	warningsToFinalBan = 4
+	warningsToDeletion = 5
+	banLength = 7 * 24 * 60 * 60
+	killsBanLength = 5 * 24 * 60 * 60
+	finalBanLength = 30 * 24 * 60 * 60
+	ipBanishmentLength = 7 * 24 * 60 * 60
+	broadcastBanishments = false
+	maxViolationCommentSize = 200
+	violationNameReportActionType = 2
+	autoBanishUnknownBytes = true
+
+	worldType = "open"
+	protectionLevel = 80
+	pvpTileIgnoreLevelAndVocationProtection = true
+	pzLocked = 60 * 1000
+	huntingDuration = 60 * 1000
+	criticalHitChance = 7
+	criticalHitMultiplier = 1
+	displayCriticalHitNotify = false
+	removeWeaponAmmunition = false
+	removeWeaponCharges = true
+	removeRuneCharges = true
+	whiteSkullTime = 7.5 * 60 * 1000
+	noDamageToSameLookfeet = false
+	showHealingDamage = true
+	showHealingDamageForMonsters = false
+	fieldOwnershipDuration = 5 * 1000
+	stopAttackingAtExit = false
+	loginProtectionPeriod = 10 * 1000
+	deathLostPercent = 10
+	stairhopDelay = 2 * 1000
+	pushCreatureDelay = 2 * 1000
+	deathContainerId = 1987
+	gainExperienceColor = 215
+	addManaSpentInPvPZone = true
+	squareColor = 0
+	allowFightback = true
+	fistBaseAttack = 7
+
+	worldId = 0
+	ip = "192.166.219.85"
+	bindOnlyGlobalAddress = false
+	loginPort = 7171
+	gamePort = 7172
+	port = 7171
+	loginTries = 10
+	retryTimeout = 5 * 1000
+	loginTimeout = 60 * 1000
+	maxPlayers = 900
+	motd = "Welcome to the OTSL!"
+	displayOnOrOffAtCharlist = false
+	onePlayerOnlinePerAccount = true
+	allowClones = false
+	serverName = "OTSL by Leon Zawodowiec"
+	loginMessage = "Welcome to the OTSL!"
+	statusTimeout = 1 * 60 * 1000
+	replaceKickOnLogin = true
+	forceSlowConnectionsToDisconnect = true
+	loginOnlyWithLoginServer = false
+	premiumPlayerSkipWaitList = false
+	
+	rsaPrime1 = "14299623962416399520070177382898895550795403345466153217470516082934737582776038882967213386204600674145392845853859217990626450972452084065728686565928113"
+	rsaPrime2 = "7630979195970404721891201847792002125535401292779123937207447574596692788513647179235335529307251350570728407373705564708871762033017096809910315212884101"
+	rsaPublic = "65537"
+	rsaModulus = "109120132967399429278860960508995541528237502902798129123468757937266291492576446330739696001110603907230888610072655818825358503429057592827629436413108566029093628212635953836686562675849720620786279431090218017681061521755056710823876476444260558147179707119674283982419152118103759076030616683978566631413"
+	rsaPrivate = "46730330223584118622160180015036832148732986808519344675210555262940258739805766860224610646919605860206328024326703361630109888417839241959507572247284807035235569619173792292786907845791904955103601652822519121908367187885509270025388641700821735345222087940578381210879116823013776808975766851829020659073"
+
+	sqlType = "mysql"
+	sqlHost = "localhost"
+	sqlPort = 3306
+	sqlUser = "root"
+	sqlPass = ""
+	sqlDatabase = "otsl"
+	sqlFile = "theforgottenserver.s3db"
+	sqlKeepAlive = 0
+	mysqlReadTimeout = 10
+	mysqlWriteTimeout = 10
+	encryptionType = "sha1"
+
+	deathListEnabled = true
+	deathListRequiredTime = 1 * 60 * 1000
+	deathAssistCount = 19
+	maxDeathRecords = 5
+
+	ingameGuildManagement = false
+	levelToFormGuild = 150
+	premiumDaysToFormGuild = 0
+	guildNameMinLength = 4
+	guildNameMaxLength = 20
+
+	highscoreDisplayPlayers = 15
+	updateHighscoresAfterMinutes = 60
+
+	buyableAndSellableHouses = true
+	houseNeedPremium = true
+	bedsRequirePremium = true
+	levelToBuyHouse = 150
+	housesPerAccount = 1
+	houseRentAsPrice = false
+	housePriceAsRent = false
+	housePriceEachSquare = 1000
+	houseRentPeriod = "week"
+	houseCleanOld = 0
+	guildHalls = true
+
+	timeBetweenActions = 200
+	timeBetweenExActions = 1000
+	hotkeyAimbotEnabled = true
+
+	mapName = "otsl.otbm"
+	mapAuthor = "Leon Zawodowiec"
+	randomizeTiles = true
+	storeTrash = true
+	cleanProtectedZones = true
+	mailboxDisabledTowns = ""
+
+	defaultPriority = "high"
+	niceLevel = 5
+	coresUsed = "-1"
+
+	startupDatabaseOptimization = true
+	updatePremiumStateAtStartup = true
+	confirmOutdatedVersion = false
+
+	formulaLevel = 2.0
+	formulaMagic = 1.0
+	bufferMutedOnSpellFailure = false
+	spellNameInsteadOfWords = false
+	emoteSpells = false
+	unifiedSpells = true
+
+	allowChangeOutfit = true
+	allowChangeColors = true
+	allowChangeAddons = true
+	disableOutfitsForPrivilegedPlayers = false
+	addonsOnlyPremium = true
+
+	dataDirectory = "data/"
+	logsDirectory = "data/logs/"
+	bankSystem = true
+	displaySkillLevelOnAdvance = true
+	promptExceptionTracerErrorBox = true
+	maximumDoorLevel = 500
+	maxMessageBuffer = 4
+	tradeLimit = 100
+
+	separateVipListPerCharacter = false
+	vipListDefaultLimit = 20
+	vipListDefaultPremiumLimit = 100
+
+	saveGlobalStorage = true
+	useHouseDataStorage = true
+	storePlayerDirection = true
+
+	checkCorpseOwner = true
+	monsterLootMessage = 3
+	monsterLootMessageType = 25
+
+	ghostModeInvisibleEffect = false
+	ghostModeSpellEffects = false
+
+	idleWarningTime = 59 * 60 * 1000
+	idleKickTime = 60 * 60 * 1000
+	reportsExpirationAfterReads = 1
+	playerQueryDeepness = 2
+	tileLimit = 20
+	protectionTileLimit = 0
+	houseTileLimit = 0
+
+	freePremium = false
+	premiumForPromotion = true
+
+	blessings = true
+	blessingOnlyPremium = true
+	blessingReductionBase = 30
+	blessingReductionDecrement = 5
+	eachBlessReduction = 8
+
+	experienceStages = false
+	rateExperience = 1.0
+	rateExperienceFromPlayers = 0
+	rateSkill = 30.0
+	rateMagic = 24.0
+	rateLoot = 2.0
+	rateSpawn = 1
+
+	rateMonsterHealth = 1.0
+	rateMonsterMana = 1.0
+	rateMonsterAttack = 1.0
+	rateMonsterDefense = 1.0
+
+	minLevelThresholdForKilledPlayer = 0.9
+	maxLevelThresholdForKilledPlayer = 1.1
+
+	rateStaminaLoss = 1
+	rateStaminaGain = 3
+	rateStaminaThresholdGain = 12
+	staminaRatingLimitTop = 40 * 60
+	staminaRatingLimitBottom = 14 * 60
+	staminaLootLimit = 14 * 60
+	rateStaminaAboveNormal = 1.5
+	rateStaminaUnderNormal = 0.5
+	staminaThresholdOnlyPremium = true
+
+	experienceShareRadiusX = 30
+	experienceShareRadiusY = 30
+	experienceShareRadiusZ = 1
+	experienceShareLevelDifference = 2 / 3
+	extraPartyExperienceLimit = 20
+	extraPartyExperiencePercent = 5
+	experienceShareActivity = 2 * 60 * 1000
+
+	globalSaveEnabled = false
+	globalSaveHour = 8
+	globalSaveMinute = 0
+	shutdownAtGlobalSave = true
+	cleanMapAtGlobalSave = false
+
+	deSpawnRange = 2
+	deSpawnRadius = 50
+
+	maxPlayerSummons = 2
+	teleportAllSummons = false
+	teleportPlayerSummons = false
+
+	statusPort = 7171
+	ownerName = "God Leonidas"
+	ownerEmail = ""
+	url = "http://192.166.219.85/"
+	location = "Poland"
+	displayGamemastersWithOnlineCommand = false
+
+	displayPlayersLogging = true
+	prefixChannelLogs = ""
+	runFile = "OTSL_r.log"
+	outputLog = "OTSL_debug.log"
+	truncateLogOnStartup = false
+
+	managerPort = 7171
+	managerLogs = true
+	managerPassword = ""
+	managerLocalhostOnly = true
+	managerConnectionsLimit = 1
+
+	adminPort = 7171
+	adminLogs = true
+	adminPassword = ""
+	adminLocalhostOnly = true
+	adminConnectionsLimit = 1
+	adminRequireLogin = true
+	adminEncryption = ""
+	adminEncryptionData = ""
