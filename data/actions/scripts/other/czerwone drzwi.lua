ab9af9e79bf28d44b365592150be32d6faef96e6
Leon Zawodowiec <leonzawodowiec1532@yahoo.com>
2013-06-02 06:04:21 +0200

    Czerwone drzwi.  xD

--- data/actions/scripts/other/czerwone drzwi.lua
+++ data/actions/scripts/other/czerwone drzwi.lua
@@ -1,17 +1,16 @@
-local id = 42261            -- the action id here
-
+local id = 42261 -- WTF? Sherlock Leon wkracza do akcji.. misja: Odszukaæ czerwone drzwi. :D
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	local cidPosition = getCreaturePosition(cid)
-		if (item.actionid == id) then
-			if cidPosition.x < toPosition.x then
-				doTransformItem(item.uid, item.itemid + 1)
-		doTeleportThing(cid, toPosition, TRUE)
+	if (item.actionid == id) then
+		if cidPosition.x < toPosition.x then
+			doTransformItem(item.uid, item.itemid + 1)
+			doTeleportThing(cid, toPosition, true)
 		else
-				if cidPosition.x > toPosition.x then
+			if cidPosition.x > toPosition.x then
 				doTransformItem(item.uid, item.itemid + 1)
-		doTeleportThing(cid, toPosition, TRUE)
-end
-end
-	return TRUE
-end
+				doTeleportThing(cid, toPosition, true)
+			end
+		end
+	end
+return true
 end
